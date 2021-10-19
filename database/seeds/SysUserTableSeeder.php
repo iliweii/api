3a3c1f846b250cfee2f70e608777e167868d4b60
iliweii <chall21@yeah.net>
2021-10-19 12:49:55 +0800

    修改填充数据

--- database/seeds/SysUserTableSeeder.php
+++ database/seeds/SysUserTableSeeder.php
@@ -17,8 +17,8 @@ class SysUserTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::insert("INSERT INTO `sys_user` (`id`, `username`, `nickname`, `password`, `salt`, `avatar`, `birthday`, `gender`, `email`, `phone`, `status`, `identity`, `remember_token`, `created_at`, `updated_at`, `deleted_at`) VALUES (1, 'admin', '管理员', 'a2661cfed1dffee1', 'ZzypRB', NULL, NULL, 0, NULL, NULL, 1, 1, NULL, NULL, NULL, NULL)");
-        DB::insert("INSERT INTO `sys_user` (`id`, `username`, `nickname`, `password`, `salt`, `avatar`, `birthday`, `gender`, `email`, `phone`, `status`, `identity`, `remember_token`, `created_at`, `updated_at`, `deleted_at`) VALUES (2, '李威', '李威', '40509a8c05fd411a', 'Sc1ePB', NULL, NULL, 0, NULL, NULL, 1, 1, NULL, NULL, NULL, NULL)");
-        DB::insert("INSERT INTO `sys_user` (`id`, `username`, `nickname`, `password`, `salt`, `avatar`, `birthday`, `gender`, `email`, `phone`, `status`, `identity`, `remember_token`, `created_at`, `updated_at`, `deleted_at`) VALUES (3, '赵丽红', '赵丽红', '36b71aa1564a76ef', '4kkMwx', NULL, NULL, 0, NULL, NULL, 1, 1, NULL, NULL, NULL, NULL)");
+        DB::insert("INSERT INTO `sys_user` (`id`, `username`, `nickname`, `password`, `salt`, `avatar`, `birthday`, `gender`, `email`, `phone`, `status`, `identity`, `remember_token`, `created_at`, `updated_at`, `deleted_at`) VALUES (1, 'admin', '管理员', '00f8b5ecaeb2e971ac2d1e4cd6a83a09', 'ZzypRB', NULL, NULL, 0, NULL, NULL, 1, 1, NULL, NULL, NULL, NULL)");
+        DB::insert("INSERT INTO `sys_user` (`id`, `username`, `nickname`, `password`, `salt`, `avatar`, `birthday`, `gender`, `email`, `phone`, `status`, `identity`, `remember_token`, `created_at`, `updated_at`, `deleted_at`) VALUES (2, '李威', '李威', '22f06496a071081240eab163c8655ff7', 'Sc1ePB', NULL, NULL, 0, NULL, NULL, 1, 1, NULL, NULL, NULL, NULL)");
+        DB::insert("INSERT INTO `sys_user` (`id`, `username`, `nickname`, `password`, `salt`, `avatar`, `birthday`, `gender`, `email`, `phone`, `status`, `identity`, `remember_token`, `created_at`, `updated_at`, `deleted_at`) VALUES (3, '赵丽红', '赵丽红', 'cb38b136ac77796bd9f3a91162ce4037', '4kkMwx', NULL, NULL, 0, NULL, NULL, 1, 1, NULL, NULL, NULL, NULL)");
     }
 }
